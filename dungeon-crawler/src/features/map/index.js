b2a4da765781e8ab395d93ab879ddce1478e6a94
Kennan <kennan.smith1@gmail.com>
2018-09-08 12:57:42 -0600

    added map component - currently a basic black rectangle

---
+++ dungeon-crawler/src/features/map/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Map(props) {
+    return (
+        <div
+            style={{
+                width: '800px',
+                height: '400px',
+                backgroundColor: 'black',
+                border: '4px solid white',
+                margin: '10px auto'
+            }}
+            >
+
+        </div>
+    )
+}
+
+export default Map
